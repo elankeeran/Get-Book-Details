ce34fa687b220241d691c65d0542f1c51e46b424
Elankeeran Vaithiyanathan <ekeeran@gmail.com>
2011-10-21 01:04:26 +0530

    add foreach to detail about the book

--- php/getBookGet.php
+++ php/getBookGet.php
@@ -6,76 +6,57 @@ $isbn=$_POST['isbn'];
 $Amazon=new Amazon();
 
 $parameters=array(
-"region"=>"com",
-"AssociateTag"=>'affiliateTag',
-"Condition"=>"All",
-"Operation"=>"ItemSearch",
-'ResponseGroup'=>'Large',
-"SearchIndex"=>"Books",
-"Keywords"=>$isbn //"145161036X"
+	"region"=>"com",
+	"AssociateTag"=>'affiliateTag',
+	"Condition"=>"All",
+	"Operation"=>"ItemSearch",
+	'ResponseGroup'=>'Large',
+	"SearchIndex"=>"Books",
+	"Keywords"=>$isbn //"145161036X"
 );
 
 $queryUrl=$Amazon->getSignedUrl($parameters);
-
 $response=simplexml_load_file($queryUrl);
-//print_r($response);
 
 if($response->Items->TotalResults > 0)
 {
-
-   // we have at least one response
-  $imagesrc=$response->Items->Item->LargeImage->URL;
-  $Author=$response->Items->Item->ItemAttributes->Author;
-  $title=$response->Items->Item->ItemAttributes->Title;
-  $publisher=$response->Items->Item->ItemAttributes->Publisher;
-  $ISBN13=$response->Items->Item->ItemAttributes->EAN;
-  $ISBN10=$response->Items->Item->ItemAttributes->ISBN;
-  $numberOfPages=$response->Items->Item->ItemAttributes->NumberOfPages;
-  $languages=$response->Items->Item->ItemAttributes->Languages->Language->Name;
-    $binding=$response->Items->Item->ItemAttributes->Binding;
-	$pDate=$response->Items->Item->ItemAttributes->PublicationDate;
-  $alt=$title ."  by ". $Author;
-$objEditorialReviews=$response->Items->Item->EditorialReviews;
+	foreach($response->Items->Item as $eachItem){
+		// we have at least one response
+		$imagesrc=$eachItem->LargeImage->URL;
+		$Author=$eachItem->ItemAttributes->Author;
+		$title=$eachItem->ItemAttributes->Title;
+		$publisher=$eachItem->ItemAttributes->Publisher;
+		$ISBN13=$eachItem->ItemAttributes->EAN;
+		$ISBN10=$eachItem->ItemAttributes->ISBN;
+		$numberOfPages=$eachItem->ItemAttributes->NumberOfPages;
+		$languages=$eachItem->ItemAttributes->Languages->Language->Name;
+		$binding=$eachItem->ItemAttributes->Binding;
+		$pDate=$eachItem->ItemAttributes->PublicationDate;
+		$alt=$title ."  by ". $Author;
+		$objEditorialReviews=$eachItem->EditorialReviews;
 ?>
-
-
-
-Title : <?php echo $title; ?>
-<br/>
-Author(s) : <?php echo $Author; ?>
-<br/>
-Publisher : <?php echo $publisher; ?>
-<br/>
-ISBN13 : <?php echo $ISBN13; ?>
-<br/>
-ISBN10 : <?php echo $ISBN10; ?>
-<br/>
-Edition Information: <?php echo $binding . ", " . $pDate ; ?>
-<br/>
-Language: <?php echo $languages; ?>
-<br/>
-Number of Page: <?php echo $numberOfPages; ?>
-<br/>
-<?php if($imagesrc!=""){ ?>
-<img src="<?php echo $imagesrc;?>"  title="<?php echo $alt; ?>" />
-<?php }else{ ?>
-	<img src="images/no_book_cover1.jpg"  title="<?php echo $alt; ?>" />
-<?php } ?>
-<br>
-
+	Title : <?php echo $title; ?>	<br/>
+	Author(s) : <?php echo $Author; ?><br/>
+	Publisher : <?php echo $publisher; ?><br/>
+	ISBN13 : <?php echo $ISBN13; ?><br/>
+	ISBN10 : <?php echo $ISBN10; ?><br/>
+	Edition Information: <?php echo $binding . ", " . $pDate ; ?><br/>
+	Language: <?php echo $languages; ?><br/>
+	Number of Page: <?php echo $numberOfPages; ?><br/>
+	<?php if($imagesrc!=""){ ?>
+		<img src="<?php echo $imagesrc;?>"  title="<?php echo $alt; ?>" />
+	<?php }else{ ?>
+		<img src="images/no_book_cover1.jpg"  title="<?php echo $alt; ?>" />
+	<?php } ?>
+	<br>
 <?php
-//print_r($objEditorialReviews);
-
-foreach($objEditorialReviews as $EditorialReview)
-{
- 
- echo "<h3>Description:</h3>";
- echo($EditorialReview->EditorialReview->Content);
-}
-
-}
-else
-{
+		foreach($objEditorialReviews as $EditorialReview)
+		{
+			echo "<h3>Description:</h3>";
+			echo($EditorialReview->EditorialReview->Content);
+		}
+	}
+}else{
 ?>
   <h3>Book not available</h3>
   <p>Please try with different books</p>
